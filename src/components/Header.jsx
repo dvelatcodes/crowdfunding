23fdc5c310881533a5f43d8ccd99b6b5db9ef8f7
love olubummo <olubummolove3@gmail.com>
2024-12-06 19:56:03 +0100

    update: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='w-screen h-[40vh]'>
-kbbkbkbk
+    <header className='w-screen h-[40vh] pt-6'>
+      <div className="w-[90vw] md:w-[80vw] m-auto flex justify-between">
+        <img src="../images/logo.svg" alt="logo" />
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Discover</a>
+          <a href="#">Get Started</a>
+        </nav>
+      </div>
     </header>
   )
 }
