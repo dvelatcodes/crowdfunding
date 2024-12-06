f740a8fcd97b6ce37941c9ebae5535bc0bd0f65f
love olubummo <olubummolove3@gmail.com>
2024-12-06 20:10:57 +0100

    update: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <header className='w-screen h-[40vh] pt-6'>
-      <div className="w-[90vw] md:w-[80vw] m-auto flex justify-between">
+      <div className="w-[90vw] md:w-[80vw] m-auto flex justify-between relative">
         <img src="/images/logo.svg" alt="logo" />
-        <nav>
+        <nav className='text-white absolute md:block flex flex-col md:flex-row w-[90vw] md:w-fit bg-white md:bg-transparent py-4 md:py-0'>
           <a href="#">About</a>
           <a href="#">Discover</a>
           <a href="#">Get Started</a>
