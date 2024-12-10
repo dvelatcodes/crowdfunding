74cf3c6e999b6080f0c74f4097c2ad12b20d60cf
love olubummo <olubummolove3@gmail.com>
2024-12-10 20:14:35 +0100

    update: header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       <div className="w-[90vw] md:w-[80vw] m-auto flex justify-between relative z-10">
         <img src="/images/logo.svg" alt="logo" />
         <nav className='text-black md:text-white absolute z-30 md:relative flex flex-col md:flex-row w-[90vw] md:w-fit bg-white md:bg-transparent py-6 md:py-0 top-32 md:top-0 pl-6 md:pl-0 gap-y-8 md:gap-y-0 md:gap-x-6 lg:gap-x-8 xl:gap-x-12 rounded-lg font-medium'>
-          <a href="#">About</a>
-          <a href="#">Discover</a>
+          <a href="#" className='relative'>About</a>
+          <a href="#" className='relative'>Discover</a>
           <a href="#">Get Started</a>
         </nav>
       </div>
