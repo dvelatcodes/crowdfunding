742460e1b91c508233a6191194c385cfd294cc2a
love olubummo <olubummolove3@gmail.com>
2024-12-10 19:50:28 +0100

    removed div tag with shades class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='w-screen h-[40vh] pt-6'>
-      <div className="shades w-full h-full absolute top-0 left-0"></div>
+    <header className='w-screen h-[40vh] pt-6 relative'>
       <div className="w-[90vw] md:w-[80vw] m-auto flex justify-between relative z-10">
         <img src="/images/logo.svg" alt="logo" />
         <nav className='text-black md:text-white absolute md:relative flex flex-col md:flex-row w-[90vw] md:w-fit bg-white md:bg-transparent py-6 md:py-0 top-12 md:top-0 pl-6 md:pl-0 gap-y-8 md:gap-y-0 md:gap-x-6 lg:gap-x-8 xl:gap-x-12 rounded-lg font-medium'>
