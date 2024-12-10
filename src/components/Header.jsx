5f1a9e541edcb162ea11447a694a287a46dec621
love olubummo <olubummolove3@gmail.com>
2024-12-10 20:37:32 +0100

    update: header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <header className='w-screen h-[40vh] pt-6'>
       <div className="shades1 w-screen h-[20vh] absolute left-0 top-0"></div>
-      <div className="shades2 absolute w-screen h-screen top-0 left-0 md:hidden"></div>
+      <div className={`${ham === 'open'? 'hidden' : 'block'} shades2 absolute w-screen h-screen top-0 left-0 md:hidden`}></div>
       <div className="w-[90vw] md:w-[80vw] m-auto flex justify-between relative z-10">
         <img src="/images/logo.svg" alt="logo" />
         <nav className={`${ham === 'open'? 'hidden' : 'flex'} text-black md:text-white absolute z-30 md:relative md:flex flex-col md:flex-row w-[90vw] md:w-fit bg-white md:bg-transparent py-6 md:py-0 top-32 md:top-0 pl-6 md:pl-0 gap-y-8 md:gap-y-0 md:gap-x-6 lg:gap-x-8 xl:gap-x-12 rounded-lg font-medium`}>
