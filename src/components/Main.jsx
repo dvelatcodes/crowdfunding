6c8e417aa45e3e000512f2d678127390c093f1c4
love olubummo <olubummolove3@gmail.com>
2024-12-20 13:47:37 +0100

    update: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,15 +6,17 @@ const Main = () => {
       <div className="masterCraft w-[90vw] md:w-[80vw]">
         <div className="w-full">
           <img src="/images/logo-mastercraft.svg" alt="mastercraft logo" />
-          <h1>
-            Mastercraft Bamboo Monitor Riser
-          </h1>
+          <h1>Mastercraft Bamboo Monitor Riser</h1>
           <p>
-          A beautiful & handcrafted monitor stand to reduce neck and eye
-          strain.
+            A beautiful & handcrafted monitor stand to reduce neck and eye
+            strain.
           </p>
           <div className="w-full">
-            
+            <button>Back this project</button>
+            <div className="">
+              <img src="/images/icon-bookmark.svg" alt="bookmark" />
+              <span>Bookmark</span>
+            </div>
           </div>
         </div>
         <div className="range"></div>
