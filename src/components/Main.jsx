eff9f30dfe50c753465066b9629b0a60536c79a7
love olubummo <olubummolove3@gmail.com>
2024-12-20 13:19:34 +0100

    update: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
   return (
-    <main>
-      <div className="w-[90vw] md:w-[80vw]"></div>
+    <main className="w-screen bg-slate-100">
+      <div className="masterCraft w-[90vw] md:w-[80vw]">
+        <div className="">
+          <img src="" alt="" />
+          <h1>
+            A beautiful & handcrafted monitor stand to reduce neck and eye
+            strain.
+          </h1>
+          <p></p>
+        </div>
+        <div className="range"></div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
