ff34ace211ffe4b37f5e50d6329cc354b399716f
love olubummo <olubummolove3@gmail.com>
2024-12-21 10:35:30 +0100

    update: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Main = () => {
   return (
     <main className="w-screen bg-slate-100">
-      <div className="masterCraft w-[90vw] md:w-[80vw] m-auto relative pt-60">
+      <div className="masterCraft w-[90vw] md:w-[60vw] lg:w-[50vw] m-auto relative pt-60">
         <div className="w-full bg-red-500 absolute left-0 right-0 -top-14 rounded-lg">
           <img src="/images/logo-mastercraft.svg" alt="mastercraft logo" />
           <h1 className="text-center">Mastercraft Bamboo Monitor Riser</h1>
