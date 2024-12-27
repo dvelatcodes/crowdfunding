231e1f3b98c0bd8970210ee7b921ed96db7d829f
love olubummo <olubummolove3@gmail.com>
2024-12-27 14:06:20 +0100

    update: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
   return (
     <main className="w-screen bg-slate-100">
       <div className="masterCraft w-[90vw] md:w-[60vw] lg:w-[50vw] m-auto relative pt-60">
-        <div className="w-full bg-red-500 absolute left-0 right-0 -top-14 rounded-lg">
+        <div className="w-full bg-white absolute left-0 right-0 -top-14 rounded-lg pt-16">
           <img src="/images/logo-mastercraft.svg" alt="mastercraft logo" className="absolute"/>
           <h1 className="text-center">Mastercraft Bamboo Monitor Riser</h1>
           <p className="text-center">
