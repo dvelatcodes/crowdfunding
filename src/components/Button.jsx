99d6f999180f694cbf7049cfa8043f544bd910fd
love olubummo <olubummolove3@gmail.com>
2024-12-28 10:18:43 +0100

    update: main component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text, color, }) => {
   return (
-    <button className='text-white' style={{backgroundColor: color}}>
+    <button className='text-white px-8 rounded-full font-semibold' style={{backgroundColor: color}}>
       {text}
     </button>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,10 +5,10 @@ const Main = () => {
   return (
     <main className="w-screen bg-slate-100">
       <div className="masterCraft w-[90vw] md:w-[60vw] lg:w-[50vw] m-auto relative pt-60">
-        <div className="w-full bg-white absolute left-0 right-0 -top-14 rounded-lg pt-12">
+        <div className="w-full bg-white absolute left-0 right-0 -top-14 rounded-lg pt-12 pb-8">
           <img src="/images/logo-mastercraft.svg" alt="mastercraft logo" className="absolute -top-7 right-0 left-0 m-auto"/>
           <h1 className="text-center text-2xl font-bold">Mastercraft Bamboo Monitor Riser</h1>
-          <p className="text-center mt-4 text-slate-500 mb-4">
+          <p className="text-center mt-4 text-slate-500 mb-6">
             A beautiful & handcrafted monitor stand to reduce neck and eye
             strain.
           </p>
