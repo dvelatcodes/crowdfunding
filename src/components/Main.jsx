ee9557d788b87502398e417cc6173363ef561709
love olubummo <olubummolove3@gmail.com>
2025-01-04 17:17:29 +0100

    update: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,16 +5,16 @@ const Main = () => {
   return (
     <main className="w-screen bg-slate-100">
       <div className="masterCraft w-[90vw] md:w-[60vw] lg:w-[50vw] m-auto relative pt-60">
-        <div className="w-full bg-white absolute left-0 right-0 -top-14 rounded-lg pt-12 pb-8">
+        <div className="w-full bg-white absolute left-0 right-0 -top-14 rounded-lg pt-12 pb-8 px-4">
           <img src="/images/logo-mastercraft.svg" alt="mastercraft logo" className="absolute -top-7 right-0 left-0 m-auto"/>
           <h1 className="text-center text-2xl font-bold">Mastercraft Bamboo Monitor Riser</h1>
           <p className="text-center mt-4 text-slate-500 mb-6">
             A beautiful & handcrafted monitor stand to reduce neck and eye
             strain.
           </p>
-          <div className="w-full flex justify-around">
+          <div className="w-full flex justify-aroun">
             <Button text="Back this project" color="hsl(176, 50%, 47%)"/>
-            <div className="">
+            <div className="md:bg-slate-100 md:flex md:gap-x-6 rounded-full md:items-center">
               <img src="/images/icon-bookmark.svg" alt="bookmark" />
               <span className="hidden md:inline">Bookmark</span>
             </div>
