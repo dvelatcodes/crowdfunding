4d694e43db1e52e0f9cf1624bebdede30fcf931e
love olubummo <olubummolove3@gmail.com>
2025-01-04 17:21:02 +0100

    update: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,11 +12,11 @@ const Main = () => {
             A beautiful & handcrafted monitor stand to reduce neck and eye
             strain.
           </p>
-          <div className="w-full flex justify-aroun">
+          <div className="w-full flex justify-around">
             <Button text="Back this project" color="hsl(176, 50%, 47%)"/>
-            <div className="md:bg-slate-100 md:flex md:gap-x-6 rounded-full md:items-center">
+            <div className="md:bg-slate-100 md:flex md:gap-x-6 md:rounded-full md:items-center md:pr-8">
               <img src="/images/icon-bookmark.svg" alt="bookmark" />
-              <span className="hidden md:inline">Bookmark</span>
+              <span className="hidden md:inline font-bold text-slate-600">Bookmark</span>
             </div>
           </div>
         </div>
