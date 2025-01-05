cd580f2651af80d8e8661a9ed524f27b292a041f
love olubummo <olubummolove3@gmail.com>
2025-01-05 05:26:27 +0100

    update: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
   };
 
   return (
-    <main className="w-screen bg-slate-100">
+    <main className="w-screen  bg-zinc-100">
       <div className="masterCraft w-[90vw] md:w-[60vw] lg:w-[50vw] m-auto relative pt-60">
         <div className="w-full bg-white absolute left-0 right-0 -top-14 rounded-lg pt-12 pb-8 px-4 md:px-0">
           <img
@@ -20,14 +20,14 @@ const Main = () => {
           <h1 className="text-center text-2xl font-bold">
             Mastercraft Bamboo Monitor Riser
           </h1>
-          <p className="text-center mt-4 text-slate-500 mb-6">
+          <p className="text-center mt-4 text-zinc-500 mb-6">
             A beautiful & handcrafted monitor stand to reduce neck and eye
             strain.
           </p>
           <div className="w-11/12 flex justify-between md:pl-8 xl:pl-12">
             <Button text="Back this project" swapColor="swapColor" />
             <div
-              className="md:bg-slate-100 md:flex md:gap-x-6 md:rounded-full md:items-center md:pr-8 cursor-pointer"
+              className="md:bg-zinc-100 md:flex md:gap-x-6 md:rounded-full md:items-center md:pr-8 cursor-pointer"
               onClick={setBookmark}
             >
               <svg width="56" height="56" xmlns="http://www.w3.org/2000/svg">
@@ -46,7 +46,7 @@ const Main = () => {
               </svg>
               <span
                 className={`hidden md:inline font-bold ${
-                  bookmarkOn ? "text-teal-600" : "text-slate-600"
+                  bookmarkOn ? "text-teal-600" : "text-zinc-600"
                 }`}
               >
                 {bookmarkOn ? "Bookmarked" : "Bookmark"}
@@ -58,15 +58,15 @@ const Main = () => {
           <div className="flex flex-col md:flex-row gap-y-4 md:gap-y-0 md:gap-x-6 lg:gap-x-12 xl:gap-x-28 2xl:gap-x-36 items-center">
             <div className="">
               <h2 className="text-center md:text-left text-3xl mb-3 font-bold">$89,914</h2>
-              <span className="text-center md:text-left text-slate-500">of $100,000 backed</span>
+              <span className="text-center md:text-left text-zinc-500">of $100,000 backed</span>
             </div>
             <div className="">
               <h2 className="text-center md:text-left text-3xl mb-3 font-bold">5,007</h2>
-              <span className="text-center md:text-left text-slate-500">total backers</span>
+              <span className="text-center md:text-left text-zinc-500">total backers</span>
             </div>
             <div className="">
               <h2 className="text-center md:text-left text-3xl mb-3 font-bold">56</h2>
-              <span className="text-center md:text-left text-slate-500">days left</span>
+              <span className="text-center md:text-left text-zinc-500">days left</span>
             </div>
           </div>
           <div className="w-10/12 md:w-11/12 h-[1.5vh] rounded-lg bg-slate-100 mt-6 m-auto md:ml-0 md:mr-0">
