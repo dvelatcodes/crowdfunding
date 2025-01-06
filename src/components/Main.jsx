81c2f38edb32e2b71645c6e8662707901a25f492
love olubummo <olubummolove3@gmail.com>
2025-01-06 23:08:29 +0100

    update: main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -85,7 +85,17 @@ const Main = () => {
             <div className="h-full w-9/12 bg-teal-500 rounded-lg"></div>
           </div>
         </div>
-        <div className="About w-full bg-white py-6 md:pl-8 xl:pl-12"></div>
+        <div className="About w-full bg-white py-6 md:pl-8 xl:pl-12">
+          <h3>
+
+          </h3>
+          <p>
+
+          </p>
+          <p>
+            
+          </p>
+        </div>
       </div>
       <div className=""></div>
     </main>
