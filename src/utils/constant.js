070b8d6383169fd4ddb866bdf243e7228e5414e5
love olubummo <olubummolove3@gmail.com>
2025-01-09 23:59:14 +0100

    update: constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,19 +2,19 @@ export const products = [
     {
         title: "Bamboo Stand",
         description: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list.",
-        price: "101",
+        price: 101,
         pledge: "$25"
     },
     {
         title: "Black Edition Stand",
         description: "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
-        price: "64",
+        price: 64,
         pledge: "$75"
     },
     {
         title: "Mahogany Special Edition",
         description: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
-        price: "0",
+        price: 0,
         pledge: "$200"
     },
 ]
