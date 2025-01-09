34f59fe611da02411aac22108a4f7f46b29fc261
love olubummo <olubummolove3@gmail.com>
2025-01-09 23:57:54 +0100

    update: constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -13,7 +13,7 @@ export const products = [
     },
     {
         title: "Mahogany Special Edition",
-        description: "",
+        description: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
         price: "",
         pledge: "$200"
     },
